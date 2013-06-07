ee20a4bd435654763e49fe33ed16732197743ed4
Alex <aleks.politic@gmail.com>
2013-06-07 18:13:19 +0300

    listeners added on error/abort/suspend events in htmlSound

--- sources/web/js/utils/htmlSound.js
+++ sources/web/js/utils/htmlSound.js
@@ -4,7 +4,7 @@
 
 var htmlSound = function() {
 	this.soundOffset = 0;
-	this.mp3offset = 0.001;//;-0.05;
+	this.mp3offset = 0.001;// ;-0.05;
 	this.audioSpriteInstance = {};
 	this.fade = false;
 
@@ -15,21 +15,21 @@ var htmlSound = function() {
 htmlSound.prototype.play = function(sndInst, callback) {
 	var that = this;
 	var spriteInst = this.audioSpriteInstance[sndInst.spriteName];
-	
+
 	if (!spriteInst || spriteInst.play) {
 		return;
 	}
-	
+
 	spriteInst.stopCallback = callback;
 	spriteInst.audio.volume = sndInst.volume;
 	spriteInst.audio.pause();
-	if(sndInst.loop){
+	if (sndInst.loop) {
 		spriteInst.audio.addEventListener('ended', function() {
-		    this.currentTime = 0;
-		    this.play();
+			this.currentTime = 0;
+			this.play();
 		}, false);
 	}
-	
+
 	spriteInst.startTime = sndInst.offset + this.soundOffset;
 	spriteInst.endTime = spriteInst.startTime + sndInst.duration;
 	spriteInst.audio.currentTime = spriteInst.startTime;
@@ -42,46 +42,46 @@ htmlSound.prototype.stop = function(sndInst) {
 	if (this.audioSpriteInstance == null) {
 		return;
 	}
-	if(sndInst){
-		if(!this.audioSpriteInstance[sndInst.spriteName]){
+	if (sndInst) {
+		if (!this.audioSpriteInstance[sndInst.spriteName]) {
 			return;
 		}
 		this.audioSpriteInstance[sndInst.spriteName].audio.pause();
 		this.audioSpriteInstance[sndInst.spriteName].play = false;
-	}else{
-		$['each'](this.audioSpriteInstance, function(index, value){
+	} else {
+		$['each'](this.audioSpriteInstance, function(index, value) {
 			value.audio.pause();
 			value.play = false;
 		});
 	}
-//	this.audioSpriteInstance.pause();
+	// this.audioSpriteInstance.pause();
 };
 
 htmlSound.prototype.mute = function(channel) {
 	if (this.audioSpriteInstance == null) {
 		return;
 	}
-	if(channel){
+	if (channel) {
 		this.audioSpriteInstance[channel.playing.spriteName].audio.muted = true;
 		this.audioSpriteInstance[channel.playing.spriteName].muted = true;
-	}else{
-		$['each'](this.audioSpriteInstance, function(index, value){
+	} else {
+		$['each'](this.audioSpriteInstance, function(index, value) {
 			value.audio.muted = true;
 			value.muted = true;
 		});
 	}
-//	this.stop();
+	// this.stop();
 };
 
 htmlSound.prototype.unmute = function(channel) {
 	if (this.audioSpriteInstance == null) {
 		return;
 	}
-	if(channel){
+	if (channel) {
 		this.audioSpriteInstance[channel.playing.spriteName].audio.muted = false;
 		this.audioSpriteInstance[channel.playing.spriteName].muted = false;
-	}else{
-		$['each'](this.audioSpriteInstance, function(index, value){
+	} else {
+		$['each'](this.audioSpriteInstance, function(index, value) {
 			value.audio.muted = false;
 			value.muted = false;
 		});
@@ -90,48 +90,45 @@ htmlSound.prototype.unmute = function(channel) {
 
 htmlSound.prototype.fadeTo = function(fadeInst) {
 	var fadeStep = 10;
-	if(this.fade == fadeInst.sndInst.id){
+	if (this.fade == fadeInst.sndInst.id) {
 		return;
 	}
-	
+
 	var audio = this.audioSpriteInstance[fadeInst.sndInst.spriteName].audio;
-	if(this.audioSpriteInstance[fadeInst.sndInst.spriteName].muted){
+	if (this.audioSpriteInstance[fadeInst.sndInst.spriteName].muted) {
 		return;
 	}
 	this.fade = fadeInst.sndInst.id;
 	var that = this;
 	fadeInst.dVol = fadeInst.volume - audio.volume;
-	if(fadeInst.dVol == 0){
+	if (fadeInst.dVol == 0) {
 		return;
 	}
-	fadeInst.dVol /= fadeInst.time/fadeStep;
+	fadeInst.dVol /= fadeInst.time / fadeStep;
 	if (fadeInst.sndInst) {
 		this.fading = true;
-		var int = setInterval(function(){
-			if(Math.abs(audio.volume - fadeInst.volume) >= Math.abs(fadeInst.dVol)){
+		var int = setInterval(function() {
+			if (Math.abs(audio.volume - fadeInst.volume) >= Math.abs(fadeInst.dVol)) {
 				audio.volume += fadeInst.dVol;
-			}else{
+			} else {
 				audio.volume = fadeInst.volume;
 				that.fade = false;
-				if(fadeInst.callback){
+				if (fadeInst.callback) {
 					fadeInst.callback();
 				}
 				clearInterval(int);
 			}
-		},fadeStep);
+		}, fadeStep);
 	}
 };
-
 
 htmlSound.prototype.loadSound = function(audioSpriteName, callback) {
 	var canPlayMp3, canPlayOgg = null;
 	var myAudio = document.createElement('audio');
-//	myAudio.preload = "auto";
+	// myAudio.preload = "auto";
 	if (myAudio.canPlayType) {
-		canPlayMp3 = !!myAudio.canPlayType
-				&& "" != myAudio.canPlayType('audio/mpeg');
-		canPlayOgg = !!myAudio.canPlayType
-				&& "" != myAudio.canPlayType('audio/ogg; codecs="vorbis"');
+		canPlayMp3 = !!myAudio.canPlayType && "" != myAudio.canPlayType('audio/mpeg');
+		canPlayOgg = !!myAudio.canPlayType && "" != myAudio.canPlayType('audio/ogg; codecs="vorbis"');
 	}
 	var ext;
 	if (canPlayOgg) {
@@ -145,23 +142,39 @@ htmlSound.prototype.loadSound = function(audioSpriteName, callback) {
 	audio.preload = "auto";
 	var that = this;
 	if (callback) {
+
+		audio.addEventListener('abort', function() {
+			console.log("AAAABBBBOOOORRRRTTT", audioSpriteName);
+			alert(audioSpriteName + " aborted");
+		}, true);
+
+		audio.addEventListener('error', function() {
+			console.log("EEEERRRRROOOOOORRRRRRRRRR", audioSpriteName);
+			alert(audioSpriteName + " error");
+		}, true);
+
+		audio.addEventListener('suspend', function() {
+			console.log("SSSSSSSUUUUUUUUUUUUUUUSSSSSS", audioSpriteName);
+			alert(audioSpriteName + " suspend");
+		}, true);
+
 		var canplay = function() {
 			that.audioSpriteInstance[audioSpriteName] = {
-					audio : audio,
-					startTime : 0,
-					endTime : 0
-				};
-				console.log("GHbdtn!!");
-				callback(that.audioSpriteInstance[audioSpriteName]);
-				audio.removeEventListener("canplaythrough", canplay, false);
+				audio : audio,
+				startTime : 0,
+				endTime : 0
 			};
-		audio.addEventListener('canplaythrough',canplay , false);
+			console.log("GHbdtn!!");
+			callback(that.audioSpriteInstance[audioSpriteName]);
+			audio.removeEventListener("canplaythrough", canplay, false);
+		};
+		audio.addEventListener('canplaythrough', canplay, false);
 		audio.addEventListener('timeupdate', function() {
 			var spriteInst = that.audioSpriteInstance[audioSpriteName];
-			if(spriteInst.audio.currentTime < spriteInst.startTime) {
+			if (spriteInst.audio.currentTime < spriteInst.startTime) {
 				spriteInst.audio.currentTime = spriteInst.startTime;
 			}
-			if(spriteInst.audio.currentTime >= spriteInst.endTime) {
+			if (spriteInst.audio.currentTime >= spriteInst.endTime) {
 				spriteInst.audio.pause();
 				spriteInst.play = false;
 				if (spriteInst.stopCallback) {
