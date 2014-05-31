802a172accdbeb6c7bfb45f33837f149ad53ff37
VovaDev <vovadev@gmail.com>
2014-05-31 19:15:56 +0300

    Native stuff

--- sources/web/js/core/Physics/Physics.js
+++ sources/web/js/core/Physics/Physics.js
@@ -1,18 +1,19 @@
 var DEFAULT_B2WORLD_RATIO = 1;
 
-
-b2Math = Box2D.Common.Math.b2Math;
-b2Vec2 = Box2D.Common.Math.b2Vec2;
-b2BodyDef = Box2D.Dynamics.b2BodyDef;
-b2Body = Box2D.Dynamics.b2Body;
-b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
-b2Fixture = Box2D.Dynamics.b2Fixture;
-b2World = Box2D.Dynamics.b2World;
-b2MassData = Box2D.Collision.Shapes.b2MassData;
-b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
-b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
-b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
-b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef;
+if (typeof(Native) == "undefined") {
+	b2Math = Box2D.Common.Math.b2Math;
+	b2Vec2 = Box2D.Common.Math.b2Vec2;
+	b2BodyDef = Box2D.Dynamics.b2BodyDef;
+	b2Body = Box2D.Dynamics.b2Body;
+	b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
+	b2Fixture = Box2D.Dynamics.b2Fixture;
+	b2World = Box2D.Dynamics.b2World;
+	b2MassData = Box2D.Collision.Shapes.b2MassData;
+	b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
+	b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
+	b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
+	b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef;
+}
 
 // TODO: remove?
 function boxPolyVertices(positionX, positionY, extentionX, extentionY) {
